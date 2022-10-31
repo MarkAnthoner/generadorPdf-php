b67905b62b623712eb8149dc1b0db2addfb8a3c5
MarkAnthoner <54249176+MarkAnthoner@users.noreply.github.com>
2022-10-31 14:18:29 -0600

    Archivos del proyecto

---
+++ appPdf/app/plantillas/plantilla2.php
@@ -0,0 +1,37 @@
+
+<?php
+
+function getPlantilla(){
+
+
+$plantilla =
+    '<body>
+        <div class="container">
+            <div class="logo">
+                An Organization
+            </div>
+
+            <div class="marquee">
+                Certificate of Completion
+            </div>
+
+            <div class="assignment">
+                This certificate is presented to
+            </div>
+
+            <div class="person">
+                Joe Nathan
+            </div>
+
+            <div class="reason">
+                For deftly defying the laws of gravity<br/>
+                and flying high
+            </div>
+        </div>
+    </body>';
+
+    return $plantilla;
+
+    }
+
+?>
