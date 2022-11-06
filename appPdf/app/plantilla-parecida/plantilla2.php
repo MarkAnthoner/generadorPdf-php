10d32f4d0964302e8fa69bf7775f655ff854e16f
MarkAnthoner <54249176+MarkAnthoner@users.noreply.github.com>
2022-11-06 00:45:41 -0600

    Plantilla parecida en MPDF
    Faltan ajustes de la plantilla

---
+++ appPdf/app/plantilla-parecida/plantilla2.php
@@ -0,0 +1,46 @@
+
+<?php
+
+function getPlantilla(){
+
+
+$plantilla =
+    '<body>
+    <div class="container">
+            <div class="logo">
+                
+            </div>
+
+            <div class="marquee">
+                Constancia
+
+                <div class="assignment">
+                    This certificate is presented to
+                </div>
+
+                <div class="person">
+                    Hola mundo
+                </div>
+
+            </div>
+
+            
+
+            <div class="reason">
+                <div class="caja">
+                        M.I. Gerard
+                        Lorem ipsum, dolor sit amet consectetur
+                    </div>
+                    <div class="caja">
+                        ING. Henry
+                        Lorem ipsum, dolor sit amet consectetur
+                    </div>
+            </div>
+        </div>
+</body>';
+
+    return $plantilla;
+
+    }
+
+?>
