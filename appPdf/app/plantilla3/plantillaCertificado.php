e3e1977e992b6ef70002b6619a091d98632e92ee
Joz Fama <jozfama@MacBook-Pro-de-Joz.local>
2022-11-07 15:49:41 -0600

    añadiendo plantilla para firmas

--- appPdf/app/plantilla3/plantillaCertificado.php
+++ appPdf/app/plantilla3/plantillaCertificado.php
@@ -14,17 +14,24 @@
                             <div class="marquee">
                                 <h1>Constancy</h1>
                             </div>
-        
                             <div class="person">
                                 <h2>Joe Nathan</h2>
                             </div>
-        
                             <div class="assignment">
                                 <h5>For his excellence and wonderful:</h5>
                             </div>
-        
                             <div class="reason">
                                 <h3>deftly defying the laws of gravity and flying high</h3>
+                            </div>
+                            <div class="firmas">
+                                <div class="firma1">
+                                    <p class="nombre">M.I. GERARDO RUIZ SOLORIO</p>
+                                    <p class="ocupacion">SECRETARIO GENERAL DE LA FACULTAD DE INGENIERIA</p>
+                                </div>
+                                <div class="firma2">
+                                    <p class="nombre">ING. ENRIQUE BARRANCO VITE</p>
+                                    <p class="ocupacion">COORDINADOR DE LA UNIDAD DE SERVICIOS DE CÓMPUTO ACADÉMICO</p>
+                                </div>
                             </div>
                         </div>
                     </div>
