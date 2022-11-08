13fc24940b9aec492fb15780463394ad627bb9e8
Joz Fama <jozfama@MacBook-Pro-de-Joz.local>
2022-11-08 13:30:53 -0600

    Archivo para la configuracion de la conexion a la BD

---
+++ appPdf/app/configBD.php
@@ -0,0 +1,10 @@
+<?php
+// Datos para acceder a la base de datos
+// Modificar los datos de acuerdo a su usuario y contraseña en postgres
+// Respetar el espacio al final de los valores
+$server = "host=localhost ";
+$username = "user= ";
+$password = "password= ";
+$dbname = "dbname= ";
+$post = $server . $dbname . $username . $password;
+?>
