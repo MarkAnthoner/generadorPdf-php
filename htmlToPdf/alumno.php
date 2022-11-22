e7f6bbb6e04d8de5fc3e870c49e1cb3caa37275a
Joz Fama MW <jozeta_fama@hotmail.com>
2022-11-21 20:13:43 -0600

    App del alumno

---
+++ htmlToPdf/alumno.php
@@ -0,0 +1,35 @@
+<?php
+    $alumno = "Nombre Alumno"
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Alumno</title>
+</head>
+<body>
+    <h1><?php echo $alumno ?></h1><br>
+    <hr>
+    <h2>Consultar constancias</h2><br>
+    <table>
+        <tr>
+            <th>Período</th>
+            <th>Fecha de emisión</th>
+            <th>Tipo de constancia</th>
+            <th>Calificacion</th>
+            <th>Ver constancia</th>
+        </tr>
+        <tr>
+            <td>x</td>
+            <td>x</td>
+            <td>x</td>
+            <td>x</td>
+            <td>x</td>
+        </tr>
+    </table>
+</body>
+</html>
