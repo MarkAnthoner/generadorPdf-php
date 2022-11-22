0dddf20e83fc5d7aa796d3ed42e7b6a4b96596f7
Joz Fama MW <jozeta_fama@hotmail.com>
2022-11-21 20:14:13 -0600

    Formulario para la creacion del pdf

---
+++ htmlToPdf/formulario.php
@@ -0,0 +1,73 @@
+<!--
+    Formulario para generar la constancia en pdf
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Formulario para constancia</title>
+</head>
+<body>
+    <h1>Generar constancia</h1>
+    <hr>
+    <form class="formulario-constancia" name="formulario" method="post" action="post_constancy.php">
+        <br>
+        <label>Imagen de fondo:</label>
+        <input list="Fondo" name="fondo">
+        <datalist id="Fondo">
+            <option value="Fondo1">
+            <option value="Fondo2">
+            <option value="Fondo3">
+        </datalist>
+        <br>
+
+        <br>
+        <label>Logos:</label>
+        <input type="checkbox" name="logos[]" value="FI">FI</input>
+        <input type="checkbox" name="logos[]" value="UNAM">UNAM</input>
+        <input type="checkbox" name="logos[]" value="UNICA">UNICA</input>
+        <br>
+
+        <br>
+        <label>Nombre del estudiante:</label>
+        <input list="Nombres" name="nombre">
+        <datalist id="Nombres">
+            <option value="Nombre1">
+            <option value="Nombre2">
+            <option value="Nombre3">
+        </datalist>
+        <br>
+
+        <br>
+        <label>Mensaje para el estudiante:</label>
+        <input type="text" name="mensaje">
+        <br>
+
+        <br>
+        <label>Quien imparte los Cursos:</label>
+        <input type="text" name="imparte">
+        <br>
+
+        <br>
+        </datalist>
+        <label>Firma 1:</label>
+        <input list="Firma" name="firma">
+        <datalist id="Firma">
+            <option value="Firma1">
+            <option value="Firma2">
+            <option value="Firma3">
+        </datalist>
+        <br>
+
+        <br>
+        <label>Folio:</label>
+        <input type="text" name="folio">
+        <br>
+        <br>
+        <input type="submit"/>
+    </form>
+</body>
+</html>
