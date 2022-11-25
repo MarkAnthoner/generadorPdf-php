537e6ace2d86a27ddf38588864885aea967a287b
unknown <arturo.ad2203@comunidad.unam.mx>
2022-11-25 14:33:39 -0600

    diseño
    diseño

--- htmlToPdf/administrador.php
+++ htmlToPdf/administrador.php
@@ -9,16 +9,31 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css"> 
+    <link rel="stylesheet" href="administrador.css"> 
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Administrador</title>
 </head>
 <body>
-    <h1><?php echo $nombre?> !Bienvenido!</h1>
-    <hr>
-    <ul class="opciones">
-        <li><a href="formulario.php">Crear constancia</a></li>
-        <li><a href="#">Editar constancia</a></li>
-        <li><a href="#">Historico de constancias</a></li>
-        <li><a href="#">Administrar firma</a></li>
-    </ul>
+    <header> 
+        <img src="./src/UNICA.png" alt="" height="101px">
+        <label class="unica">Unidad de Servicios de Cómputo Académico</label>
+        <img src="./src/FI.png" alt="logo fi" height="101px">
+        <img src="./src/UNAM.png" alt="" height="101px">
+    </header>
+    <br>
+    <nav class="navbar bg-light">
+        <div id = "bienvenida"><?php echo $nombre?> !Bienvenido!</div>
+    </nav>
+    <br>
+    <div id = "acciones" class="admin">
+            <ul class="opciones">
+                <lo><a href="formulario.php" class = "opciones">Crear constancia</a></lo><br>
+                <lo><a href="#" class = "opciones">Editar constancia</a></lo><br>
+                <lo><a href="#" class = "opciones">Historico de constancias</a></lo><br>
+                <lo><a href="#" class = "opciones">Administrar firma</a></lo><br>
+            </ul>
+    </div>
+    
 </body>
 </html>
--- htmlToPdf/formulario.php
+++ htmlToPdf/formulario.php
@@ -8,86 +8,141 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="formulario.css"> 
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Formulario para constancia</title>
 </head>
 <body>
-    <h1>Generar constancia</h1>
+    <header> 
+        <img src="./src/UNICA.png" alt="" height="101px">
+        <label class="unica">Unidad de Servicios de Cómputo Académico</label>
+        <img src="./src/FI.png" alt="logo fi" height="101px">
+        <img src="./src/UNAM.png" alt="" height="101px">
+    </header>
+
+    <div class = "tit"><p>Generar constancia</p></div>
+    
     <hr>
-    <form class="formulario-constancia" name="formulario" method="post" action="post_constancy.php">
-        <br>
-        <label>Imagen de fondo:</label>
-        <input list="Fondo" name="fondo">
-        <datalist id="Fondo">
-            <option value="Fondo1">
-            <option value="Fondo2">
-            <option value="Fondo3">
-        </datalist>
-        <br>
+    <div class="d-flex flex-column mb-3">
+        <div class="d-flex justify-content-center" id="admin">
+            <br>
+            <table border = "none">
+                <form class="formulario-constancia" name="formulario" method="post" action="post_constancy.php">
+                    <br>
+                    <tr>
+                        <td>
+                            <label>Imagen de fondo:</label>
+                        </td>
+                        <td>
+                            <input list="Fondo" name="fondo">
+                            <datalist id="Fondo">
+                                <option value="Fondo1">
+                                <option value="Fondo2">
+                                <option value="Fondo3">
+                            </datalist>  
+                        </td>
+                        
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Logos:</label>
+                        </td>
+                        <td>
+                            <input type="checkbox" name="logos[]" value="FI">FI</input>
+                            <input type="checkbox" name="logos[]" value="UNAM">UNAM</input>
+                            <input type="checkbox" name="logos[]" value="UNICA">UNICA</input>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Nombre del estudiante:</label>
+                        </td>
+                        <td>
+                            <input list="Nombres" name="nombre">
+                            <datalist id="Nombres">
+                                <option value="Nombre1">
+                                <option value="Nombre2">
+                                <option value="Nombre3">
+                            </datalist>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Mensaje para el estudiante:</label>
+                        </td>
+                        <td>
+                            <input type="text" name="mensaje">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Quien imparte los Cursos:</label>
+                        </td>
+                        <td>
+                            <input type="text" name="imparte">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Firma 1:</label>
+                        </td>
+                        <td>
+                            <input list="Firma" name="firma1">
+                            <datalist id="Firma">
+                                <option value="Firma1">
+                                <option value="Firma2">
+                                <option value="Firma3">
+                            </datalist>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Funcion firma 1:</label>
+                        </td>
+                        <td>
+                            <input type="text" name="funcionfirma1">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Firma 2:</label>
+                        </td>
+                        <td>
+                            <input list="Firma2" name="firma2">
+                            <datalist id="Firma2">
+                                <option value="Firma1">
+                                <option value="Firma2">
+                                <option value="Firma3">
+                            </datalist>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Funcion firma 2:</label>
+                        </td>
+                        <td>
+                            <input type="text" name="funcionfirma2">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <label>Folio:</label>
+                        </td>
+                        <td>
+                            <input type="text" name="folio">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input type="submit"/>
+                        </td>
+                    </tr>
+                </form>
+            </table>
+            
+        </div>
+    </div>
 
-        <br>
-        <label>Logos:</label>
-        <input type="checkbox" name="logos[]" value="FI">FI</input>
-        <input type="checkbox" name="logos[]" value="UNAM">UNAM</input>
-        <input type="checkbox" name="logos[]" value="UNICA">UNICA</input>
-        <br>
 
-        <br>
-        <label>Nombre del estudiante:</label>
-        <input list="Nombres" name="nombre">
-        <datalist id="Nombres">
-            <option value="Nombre1">
-            <option value="Nombre2">
-            <option value="Nombre3">
-        </datalist>
-        <br>
-
-        <br>
-        <label>Mensaje para el estudiante:</label>
-        <input type="text" name="mensaje">
-        <br>
-
-        <br>
-        <label>Quien imparte los Cursos:</label>
-        <input type="text" name="imparte">
-        <br>
-
-        <br>
-        <label>Firma 1:</label>
-        <input list="Firma" name="firma1">
-        <datalist id="Firma">
-            <option value="Firma1">
-            <option value="Firma2">
-            <option value="Firma3">
-        </datalist>
-        <br>
-
-        <br>
-        <label>Funcion firma 1:</label>
-        <input type="text" name="funcionfirma1">
-        <br>
-
-
-        <br>
-        <label>Firma 2:</label>
-        <input list="Firma2" name="firma2">
-        <datalist id="Firma2">
-            <option value="Firma1">
-            <option value="Firma2">
-            <option value="Firma3">
-        </datalist>
-        <br>
-
-        <br>
-        <label>Funcion firma 2:</label>
-        <input type="text" name="funcionfirma2">
-        <br>
-
-        <br>
-        <label>Folio:</label>
-        <input type="text" name="folio">
-        <br>
-        <br>
-        <input type="submit"/>
-    </form>
 </body>
 </html>
